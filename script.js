39c7cadcbfaeca66f820e392617196bde8a72014
alisonstardust <zakulisami@o2.pl>
2018-06-03 21:14:34 +0200

    style update, fixed first undefined card

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//to be added: sorting by type, sorting by name
+
 const app = () => {
   const pokedex = document.querySelector('.pokedex');
   const cardApiAddress = `https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json`
@@ -7,25 +9,23 @@ const app = () => {
     fetch(apiAddress)
       .then(response => response.json())
       .then(data => pokemons.push(...data.pokemon))
-      .then(console.log(pokemons))
+      //.then(console.log(pokemons))
       .then(printPokemon)
       .catch(error =>  {
         console.log('Error: ', error)});
   }
 
   const printPokemon = () => {
-    let pokemonCards;
-    console.log(pokemons[0])
+    let pokemonCards = '';
     for (var i in pokemons) {
       pokemonCards += `
-
         <div class="cardCover">
           <div class="pokemonNumber">${pokemons[i].id}</div>
           <div class="pokemonName">${pokemons[i].name}</div>
           <img src="http://www.serebii.net/pokemongo/pokemon/${pokemons[i].num}.png" class="pokemonIMG">
-          <div class="pokemonCardDescription">${pokemons[i].type}</div>
+          <div class="pokemonCardDescription">Type: ${pokemons[i].type}</div>
         </div>
-        `;
+      `;
     }
     pokedex.innerHTML = pokemonCards;
   }
